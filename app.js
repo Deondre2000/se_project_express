7e252cb98ea8229fcac5f05d0879d521fcffb70a
deondre butler <deondrebutler20@gmail.com>
2025-11-07 17:04:18 -0500

    second update

--- app.js
+++ app.js
@@ -13,12 +13,15 @@ mongoose
   })
   .catch(console.error);
 
-const routes = require("./routes");
-
 app.use(express.json());
-app.use("/api", routes);
 
-app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
+
 app.use("/", mainRouter);
 
 app.listen(PORT, () => {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
-const { getUsers, createUser, getUser } =
-  require("../controllers/user").default;
+const { getUsers, createUser, getUser } = require("../controllers/user");
+
+// Test route to verify req.user._id is set
+router.get("/me", (req, res) => {
+  res.json({ currentUser: req.user });
+});
 
 router.get("/", getUsers);
-router.get("/userId", getUser);
+router.get("/:userId", getUser);
 router.post("/", createUser);
 
 module.exports = router;
